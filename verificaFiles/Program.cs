f730c87ce01daf2cf5439303509b4a905e0af2b3
belladonnagiacomo <belladonna.giacomo@franchettisalviani.net>
2026-05-06 12:07:55 +0200

    Aggiungere i file di progetto.

---
+++ verificaFiles/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace verificaFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
