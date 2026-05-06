ec41a3fe1fa2b902a31726492eb29314b3eeedb2
belladonnagiacomo <belladonna.giacomo@franchettisalviani.net>
2026-05-06 12:34:45 +0200

    mod

--- verificaFiles/Program.cs
+++ verificaFiles/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace verificaFiles
+﻿using System.Globalization;
+
+namespace verificaFiles
 {
     internal class Program
     {
+        static void EsercizioC(string file)
+        {
+            string[] parole = ["Vinci", "Offerta", "Compra"];
+            List<string> lista = new List<string>();
+            using(StreamReader sr = new StreamReader(file))
+            {
+                string riga = sr.ReadLine();
+                riga = sr.ReadLine();
+                while (riga != null)
+                {
+                    if (riga.Contains("Vinci") == true || riga.Contains("Offerta") == true || riga.Contains("Compra") == true)
+                    {
+                        lista.Add(riga);
+                    }
+                    
+                    riga = sr.ReadLine();
+                }
+            }
+
+            using(StreamWriter sw = new StreamWriter("messaggi_sospetti.csv"))
+            {
+                sw.WriteLine();
+                for(int i = 0;  i < lista.Count; i++)
+                {
+                    sw.WriteLine(lista[i]);
+                }
+
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string fileC = "messaggi.csv";
+            EsercizioC(fileC);
+
         }
     }
 }
